b1bd176f126a327a91b61e0d1d8ee6aedc433125
David Duong <david@foxintelligence.fr>
2020-09-20 18:38:30 +0200

    API feat(routes/locations): implement new endpoint returning csv of count of location franchisees by country

--- api/src/routes/locations.js
+++ api/src/routes/locations.js
@@ -1,11 +1,32 @@
 import express from 'express';
+import { parse } from 'json2csv';
 import locations from '../models/locations';
 
 const locationsRouter = express.Router();
 
-locationsRouter.get('/locations', (req, res) => {
+locationsRouter.get('/', (req, res) => {
   const { query } = req;
   res.json(locations.find(query));
+});
+
+locationsRouter.get('/countries', (req, res) => {
+  const locationData = locations.find(req.query);
+  const data = {};
+
+  for (let index = 0; index < locationData.length; index += 1) {
+    const { countryCode } = locationData[index];
+
+    if (data[countryCode]) {
+      data[countryCode] += 1;
+    } else {
+      data[countryCode] = 1;
+    }
+  }
+
+  const formattedData = Object.entries(data);
+  const csvData = parse(formattedData);
+
+  res.header('Content-Type', 'text/csv').send(csvData);
 });
 
 export default locationsRouter;
