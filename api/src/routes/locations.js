7f41b84361ab6a169ad83896e704a49ccd5b7c4b
David Duong <david@foxintelligence.fr>
2020-09-20 20:12:50 +0200

    API refact(routes/locations): filter out locations against laposte postal codes

--- api/src/routes/locations.js
+++ api/src/routes/locations.js
@@ -1,12 +1,36 @@
 import express from 'express';
 import { parse } from 'json2csv';
+import logger from '../lib/logger';
 import locations from '../models/locations';
+import postalCodes from '../models/postal-codes';
 
 const locationsRouter = express.Router();
 
+const filterLocations = (locationData, postalCodeData) => {
+  return locationData.filter(it => postalCodeData.includes(it.postalCode));
+};
+
 locationsRouter.get('/', (req, res) => {
+  let filteredData = [];
+  let postalCodeData = [];
+
   const { query } = req;
-  res.json(locations.find(query));
+  const locationData = locations.find(query);
+
+  // TODO: to improve performance, we should cache postal codes
+  if (!postalCodes.loaded) {
+    postalCodes.once('initialized', () => {
+      postalCodeData = postalCodes.data.map(it => it.postalCode);
+      filteredData = filterLocations(locationData, postalCodeData);
+
+      res.json(filteredData);
+    });
+  } else {
+    postalCodeData = postalCodes.data.map(it => it.postalCode);
+    filteredData = filterLocations(locationData, postalCodeData);
+
+    res.json(filteredData);
+  }
 });
 
 locationsRouter.get('/countries', (req, res) => {
